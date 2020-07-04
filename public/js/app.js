d45122f1dfdb8c1abdd5e4ee36d923b43ab5620f
Brody Dingel <brodydingel795@gmail.com>
2020-07-04 02:19:59 -0500

    Check fields for undefined to fool bots

--- public/js/app.js
+++ public/js/app.js
@@ -6,11 +6,11 @@ new TypeIt('#typed-heading', {
 })
 .go();
 
-$(document).ready(function(){
+$(document).ready(() => {
   $('[data-toggle="tooltip"]').tooltip();
 });
 
-$('.navbar-nav>li>a').on('click', function(){
+$('.navbar-nav>li>a').on('click', () => {
     $('.navbar-collapse').collapse('hide');
 });
 
@@ -23,21 +23,23 @@ $(() => {
       const contacterMessage = $('#contacter-message').val();
       const data  = JSON.stringify({contacterName: contacterName, contacterEmail: contacterEmail, contacterMessage: contacterMessage});
 
-      $.ajax({
-        url: '/',
-        method: 'POST',
-        contentType: 'application/json',
-        data: data,
-        timeout: 5000,
-        success: (response) => {
-            alert("Your message has been sent!");
-        },
-        error: (response) => {
-          if (!response.success) {
-            alert("I'm sorry, but there was an issue sending your message. Please feel free to contact me directly at brody.dingel@gmail.com.");
+      if (contacterName !== undefined && contacterEmail !== undefined && contacterMessage !== undefined) {
+        $.ajax({
+          url: '/',
+          method: 'POST',
+          contentType: 'application/json',
+          data: data,
+          timeout: 5000,
+          success: (response) => {
+              alert("Your message has been sent!");
+          },
+          error: (response) => {
+            if (!response.success) {
+              alert("I'm sorry, but there was an issue sending your message. Please feel free to contact me directly at brody.dingel@gmail.com.");
+            }
           }
-        }
-      });
+        });
+      }
     });
 });
 
