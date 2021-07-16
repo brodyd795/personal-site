4283b2cee2bed8c913781bb692f68e6c8d3f0cfb
Brody Dingel <brodydingel795@gmail.com>
2021-07-15 23:07:38 -0500

    Add simple global style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,28 @@
 import React, {FC} from 'react';
+import { createGlobalStyle } from 'styled-components';
 
 interface IProps {
     Component: FC;
 }
 
-const MyApp = ({Component}: IProps) => <Component />;
+const GlobalStyle = createGlobalStyle`
+  body {
+    color: grey;
+    margin: 0;
+  }
+
+  #__next {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+`
+
+const MyApp = ({Component}: IProps) => (
+    <>
+        <Component />
+        <GlobalStyle />
+    </>
+);
 
 export default MyApp;
