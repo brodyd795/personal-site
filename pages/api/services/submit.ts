0e6630140b7912fc425079b0f54ebc5553f7f8d4
Brody Dingel <brodydingel795@gmail.com>
2021-07-24 14:08:46 -0500

    Send email with sendGrid

--- pages/api/services/submit.ts
+++ pages/api/services/submit.ts
@@ -1,5 +1,22 @@
+import sgMail from '@sendgrid/mail';
 import {FieldValues} from '../../../types/shared-types';
 
-export const submitContactForm = (values: FieldValues) => {
-	console.log(`values in service`, values);
+export const submitContactForm = ({name, email, message}: FieldValues) => {
+	sgMail.setApiKey(process.env.SENDGRID_API_KEY ?? '');
+	const msg: sgMail.MailDataRequired = {
+		to: 'brodydingel@gmail.com',
+		from: 'brody@dingel.dev',
+		subject: 'Testing email',
+		replyTo: 'brodydingel@gmail.com',
+		text: `Message from ${name} at ${email}:\n\n${message}`,
+		html: `Message from ${name} at ${email}:\n\n${message}`
+	};
+	sgMail
+		.send(msg)
+		.then(() => {
+			console.log('Email sent');
+		})
+		.catch((error) => {
+			console.error(error);
+		});
 };
