9768eb342388d0ee7065e51207d49039c2f63f74
Brody Dingel <brodydingel795@gmail.com>
2021-07-27 22:03:43 -0500

    Clean up 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,4 @@
+import React from 'react';
 import Error from 'next/error';
 
-export default function NotFound() {
-	// Opinionated: do not record an exception in Sentry for 404
-	return <Error statusCode={404} />;
-}
+export default (): React.ReactElement => <Error statusCode={404} />;
