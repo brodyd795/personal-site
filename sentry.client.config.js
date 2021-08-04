d574a8649d90d7b76b02fa95560f8b49f1ecb272
Brody Dingel <brodydingel795@gmail.com>
2021-08-03 20:30:56 -0500

    Clean up sentry configs

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,9 +1,5 @@
 import * as Sentry from '@sentry/nextjs';
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-Sentry.init({
-	dsn:
-		SENTRY_DSN ||
-		'https://eac5edec24e345839b0a57331ebb75fa@o406754.ingest.sentry.io/5878680'
-});
+Sentry.init({dsn: SENTRY_DSN});
