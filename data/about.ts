e3ce29cdf470893a2519da07f28e8a84141359bf
Brody Dingel <brodydingel795@gmail.com>
2021-09-09 15:58:24 -0500

    Populate about page with styles

---
+++ data/about.ts
@@ -0,0 +1,29 @@
+export const aboutData = [
+	`I'm a software engineer at [Hy-Vee](https://hy-vee.com). 
+	I grew up in small-town Iowa and went to Iowa State University 
+	after graduating high school in 2014. I discovered a love for 
+	Spanish and Linguistics pretty quickly (spoiler alert – non-CS 
+	background!), and I started working on the CyWrite project, 
+	an automated writing evaluation tool for ESL classrooms. I 
+	picked up some beginner programming skills along the way and 
+	graduated suma cum laude with a B.A. in World Languages and 
+	Cultures (emphasis in Spanish) and a B.A. in Linguistics.`,
+
+	`Afterwards, I started my M.A. in Applied Linguistics with 
+	a focus in Computational/Corpus Linguistics. I quickly 
+	discovered my passion for software development, which went 
+	hand-in-hand with my linguistics expertise. I built a course 
+	website to help my linguistics students learn regular 
+	expressions, and I completed a round of #100DaysOfCode. 
+	While learning to code, I also taught elementary Spanish 
+	to my 200+ students and completed my Master's thesis on 
+	L2 vocabulary acquisition and semantic relatedness.`,
+
+	`At Hy-Vee, I work with front-end React + Next.js code, 
+	back-end Node, and a smattering of other technologies in 
+	our stack (Kubernetes, Elasticsearch, Styled Components...I 
+	could go on).`,
+
+	`I currently live in Iowa with my wife, Rachel, and our 
+	daughter, Lillia. She's super cute 😉`
+];
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import styled from 'styled-components';
 
 import {Container} from '../components/container';
+import {aboutData} from '../data/about';
+
+const StyledP = styled(ReactMarkdown)`
+	line-height: 1.5;
+	font-size: 20px;
+	padding: 0 12px;
+`;
+
+const StyledHeading = styled.h2`
+	margin-top: 30px;
+	padding: 12px;
+`;
 
 const About = () => (
-	<Container headerText='About' subHeaderText='A bit more about me...'>
+	<Container headerText='About' subHeaderText=''>
 		<main>
-			<div>All about me!</div>
+			<StyledHeading>{`Hi! I'm Brody 👋🏻`}</StyledHeading>
+			{aboutData.map((paragraph) => (
+				<StyledP>{paragraph}</StyledP>
+			))}
 		</main>
 	</Container>
 );
