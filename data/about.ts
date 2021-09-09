a1d0c5550ff681abb2f496699b10d7bf73f0fcda
Brody Dingel <brodydingel795@gmail.com>
2021-09-09 16:40:09 -0500

    Update text in about page

--- data/about.ts
+++ data/about.ts
@@ -1,22 +1,22 @@
 export const aboutData = [
 	`I'm a software engineer at [Hy-Vee](https://hy-vee.com). 
 	I grew up in small-town Iowa and went to Iowa State University 
-	after graduating high school in 2014. I discovered a love for 
+	in 2014. I discovered a love for 
 	Spanish and Linguistics pretty quickly (spoiler alert – non-CS 
-	background!), and I started working on the CyWrite project, 
+	background!), and I started working on the [CyWrite project](https://github.com/chukharev/cywrite), 
 	an automated writing evaluation tool for ESL classrooms. I 
 	picked up some beginner programming skills along the way and 
-	graduated suma cum laude with a B.A. in World Languages and 
+	graduated _suma cum laude_ with a B.A. in World Languages and 
 	Cultures (emphasis in Spanish) and a B.A. in Linguistics.`,
 
-	`Afterwards, I started my M.A. in Applied Linguistics with 
+	`Afterward, I started my M.A. in Applied Linguistics with 
 	a focus in Computational/Corpus Linguistics. I quickly 
 	discovered my passion for software development, which went 
-	hand-in-hand with my linguistics expertise. I built a course 
-	website to help my linguistics students learn regular 
-	expressions, and I completed a round of #100DaysOfCode. 
+	hand-in-hand with my linguistics training. I built [a course 
+	website](https://github.com/brodyd795/course-website) to help my linguistics students learn regular 
+	expressions, and I completed a round of [#100DaysOfCode](https://github.com/brodyd795/100-days-of-code). 
 	While learning to code, I also taught elementary Spanish 
-	to my 200+ students and completed my Master's thesis on 
+	to my 200+ students and completed [my Master's thesis](/thesis.pdf) on 
 	L2 vocabulary acquisition and semantic relatedness.`,
 
 	`At Hy-Vee, I work with front-end React + Next.js code, 
