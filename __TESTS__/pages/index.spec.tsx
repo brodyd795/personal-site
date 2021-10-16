ab7811369471133ed14550add5f87d01fd9a9e9c
Brody Dingel <bdingel@hy-vee.com>
2021-10-16 05:57:40 -0500

    Add jest command

--- __TESTS__/pages/index.spec.tsx
+++ __TESTS__/pages/index.spec.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import {render, screen} from '@testing-library/react';
+import {render as rtlRender, screen} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import Index from '../../pages';
 
 describe('Index', () => {
+    const render = () => rtlRender(<Index />);
+
     test('should do stuff', async () => {
-        render(<Index />);
+        render();
 
         expect(await screen.findByText("Hi, I'm Brody.")).toBeVisible();
     });
