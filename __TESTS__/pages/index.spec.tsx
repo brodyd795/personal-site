cddac0db6abfc5b8bf5a2cd08594bdcf436ebd39
Brody Dingel <bdingel@hy-vee.com>
2021-10-16 06:46:45 -0500

    Add tests for projects

--- __TESTS__/pages/index.spec.tsx
+++ __TESTS__/pages/index.spec.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable no-await-in-loop */
 import React from 'react';
 import {render as rtlRender, screen} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
+import {projects} from '../../data/projects';
 import Index from '../../pages';
 
 describe('Index', () => {
@@ -23,5 +25,25 @@ describe('Index', () => {
 
         expect(await screen.findByText("Hi, I'm Brody.")).toBeVisible();
         expect(await screen.findByText("I'm a full-stack software engineer. I currently work as a Software Engineer at Hy-Vee on the Browse/Shop team. Nice to meet you!")).toBeVisible();
+    });
+
+    test('should show projects', async () => {
+        render();
+
+        expect(await screen.findByRole('heading', {name: 'Projects'})).toBeVisible();
+
+        for (let index = 0; index < projects.length; index += 1) {
+            const {name: projectName, link, technologies: projectTechnologies} = projects[index];
+
+            const name = await screen.findByText(projectName);
+            const image = await screen.findByAltText(projectName);
+            const card = await screen.findByTestId(`project-card-${projectName}`);
+            const technologies = await screen.findByText(projectTechnologies.join(', '));
+
+            expect(name).toBeVisible();
+            expect(technologies).toBeVisible();
+            expect(image).toBeVisible();
+            expect(card).toHaveAttribute('href', link);
+        }
     });
 });
