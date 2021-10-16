78eede324233229c4a653c47cb9e22a94df97eff
Brody Dingel <bdingel@hy-vee.com>
2021-10-16 06:13:42 -0500

    Flesh out header tests

--- __TESTS__/pages/index.spec.tsx
+++ __TESTS__/pages/index.spec.tsx
@@ -7,9 +7,21 @@ import Index from '../../pages';
 describe('Index', () => {
     const render = () => rtlRender(<Index />);
 
-    test('should do stuff', async () => {
+    test('should show header', async () => {
         render();
 
+        const homeLink = await screen.findByRole('link', {name: 'Home'});
+        const aboutLink = await screen.findByRole('link', {name: 'About'});
+        const contactLink = await screen.findByRole('link', {name: 'Contact'});
+
+        expect(homeLink).toBeVisible();
+        expect(homeLink).toHaveAttribute('href', '/');
+        expect(aboutLink).toBeVisible();
+        expect(aboutLink).toHaveAttribute('href', '/about');
+        expect(contactLink).toBeVisible();
+        expect(contactLink).toHaveAttribute('href', '/#contact');
+
         expect(await screen.findByText("Hi, I'm Brody.")).toBeVisible();
+        expect(await screen.findByText("I'm a full-stack software engineer. I currently work as a Software Engineer at Hy-Vee on the Browse/Shop team. Nice to meet you!")).toBeVisible();
     });
 });
