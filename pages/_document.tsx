9bee8335a0bef7cb12662441daeb43ad65a2af02
Brody Dingel <bdingel@hy-vee.com>
2021-10-23 06:31:36 -0500

    Document to tw

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,40 +4,11 @@ import Document, {
 	Head,
 	Main,
 	NextScript,
-	DocumentContext
 } from 'next/document';
-import {ServerStyleSheet} from 'styled-components';
 
 import {GA_TRACKING_ID} from '../lib/gtag';
 
 export default class MyDocument extends Document {
-	static async getInitialProps(ctx: DocumentContext) {
-		const sheet = new ServerStyleSheet();
-		const originalRenderPage = ctx.renderPage;
-
-		try {
-			ctx.renderPage = () =>
-				originalRenderPage({
-					enhanceApp: (App) => (props) =>
-						sheet.collectStyles(<App {...props} />)
-				});
-
-			const initialProps = await Document.getInitialProps(ctx);
-
-			return {
-				...initialProps,
-				styles: (
-					<>
-						{initialProps.styles}
-						{sheet.getStyleElement()}
-					</>
-				)
-			};
-		} finally {
-			sheet.seal();
-		}
-	}
-
 	render() {
 		return (
 			<Html lang='en'>
