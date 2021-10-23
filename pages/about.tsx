795b158d24ad1bfa3f01f5eeca2af588e58008c9
Brody Dingel <bdingel@hy-vee.com>
2021-10-23 06:31:43 -0500

    About to tw

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,34 +1,17 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
-import styled from 'styled-components';
 
 import {Container} from '../components/container';
 import {aboutData} from '../data/about';
 import {Contact} from '../components/contact';
 
-const StyledP = styled(ReactMarkdown)`
-	line-height: 1.5;
-	font-size: 20px;
-	padding: 0 12px;
-
-	a:visited {
-		color: #1d1e22;
-	}
-`;
-
-const StyledHeading = styled.h2`
-	margin-top: 32px;
-	padding: 12px;
-	font-size: 32px;
-`;
-
 const About = () => (
 	<Container headerText='About' subHeaderText=''>
 		<main>
 			{/* eslint-disable-next-line react/no-unescaped-entities */}
-			<StyledHeading>Hi! I'm Brody 👋🏻</StyledHeading>
+			<h2>Hi! I'm Brody 👋🏻</h2>
 			{aboutData.map((paragraph) => (
-				<StyledP key={paragraph}>{paragraph}</StyledP>
+				<ReactMarkdown key={paragraph}>{paragraph}</ReactMarkdown>
 			))}
 			<Contact />
 		</main>
