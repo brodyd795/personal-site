23c69ed398c2669dd0c4142df13290cf90a38057
Brody Dingel <bdingel@hy-vee.com>
2022-01-18 23:29:01 -0600

    WIP: getting projects in a better spot

--- components/projects.tsx
+++ components/projects.tsx
@@ -9,32 +9,32 @@ interface IProjectCard {
 }
 
 const ProjectCard: FC<IProjectCard> = ({
-	project: {name, link, technologies, description}
+	project: {name, link, technologies, description, tldr, image: projectImage}
 }: IProjectCard) => (
 	<a
 		href={link}
 		target='_blank'
 		rel='noreferrer'
 		data-testid={`project-card-${name}`}
-		className='max-h-52'
 	>
-		<div className='flex flex-col border-zinc-500 border-2 m-1 p-2'>
+		<div className='flex flex-col border-zinc-500 border-2 m-1 p-2 ml-0 pl-0'>
 			<span className='text-center font-bold text-xl'>{name}</span>
-			<div className='flex'>
-				<div className='w-28 h-28'>
+			<div className='flex items-end bg-gray-900'>
+				<div className='w-28 h-36 text-center hidden sm:block'>
 					<Image
 						alt={name}
-						src='/project-images/accounting.jpg'
+						src={projectImage.src}
 						layout='responsive'
 						width={100}
 						height={100}
-						priority
 					/>
+					<span className='text-xs italic text-white'>{`Credit: ${projectImage.credit}`}</span>
 				</div>
-				<div className='flex flex-col flex-1 ml-4'>
-					<span className='mt-4 mb-auto'>{description}</span>
+				<div className='flex flex-col flex-1 pl-4 bg-blue-200'>
+					<span className='mt-4 mb-auto'><span className='font-bold'>{'TL;DR: '}</span>{tldr}</span>
+					<span className='mt-2 mb-auto'><span className='font-bold'>{'What I\'ve learned: '}</span>{description}</span>
 					{technologies ? (
-						<span className='italic mt-2'>{technologies.join(', ')}</span>
+						<span className='italic mt-2'><span className='font-bold'>{'Using: '}</span>{technologies.join(', ')}</span>
 					) : null}
 				</div>
 			</div>
--- data/projects.ts
+++ data/projects.ts
@@ -2,7 +2,12 @@ export interface IProject {
 	name: string;
 	link: string;
 	technologies: string[];
+	tldr: string;
 	description: string;
+	image: {
+		src: string;
+		credit: string;
+	}
 }
 
 export const projects: IProject[] = [
@@ -10,41 +15,66 @@ export const projects: IProject[] = [
 		name: 'Accounting',
 		link: 'https://github.com/brodyd795/accounting',
 		technologies: ['Next.js', 'MySQL', 'Styled Components'],
-		description:
-			"Custom-tailored accounting app for managing my family's finances. You know, accounting stuff."
+		tldr: "Custom-tailored for managing my family's finances. You know, accounting stuff.",
+		description: 'I\'ve gone through multiple iterations of this project, learning more and more as I go. It started in Python with Google Sheets as the backend and has now evolved into a single-page Next app powered by automated email scraping and categorization.',
+		image: {
+			src: '/project-images/accounting.jpg',
+			credit: '@tezos'
+		}
 	},
 	{
 		name: 'Course Website',
 		link: 'https://github.com/brodyd795/course-website',
 		technologies: ['React', 'Express', 'Bootstrap'],
-		description:
-			'Custom-made platform for my past students. Included an appointment scheduler, anonymous messaging, automated homework grading, and grade reports.'
+		tldr: 'Custom-made platform for my past students. Included an appointment scheduler, anonymous messaging, automated homework grading, and grade reports.',
+		description: 'I\'ve gone through multiple iterations of this project, learning more and more as I go. It started in Python with Google Sheets as the backend and has now evolved into a single-page Next app powered by automated email scraping and categorization.',
+		image: {
+			src: '/project-images/accounting.jpg',
+			credit: '@tezos'
+		}
 	},
 	{
 		name: '#100DaysOfCode',
 		link: 'https://github.com/brodyd795/100-days-of-code',
 		technologies: ['Vanilla HTML, CSS, & JS'],
-		description:
-			'Learned the basics of web dev. Would 1000% recommend to anyone looking to get their feet web in programming!'
+		tldr: 'Learned the basics of web dev. Would 1000% recommend to anyone looking to get their feet web in programming!',
+		description: 'I\'ve gone through multiple iterations of this project, learning more and more as I go. It started in Python with Google Sheets as the backend and has now evolved into a single-page Next app powered by automated email scraping and categorization.',
+		image: {
+			src: '/project-images/accounting.jpg',
+			credit: '@tezos'
+		}
 	},
 	{
 		name: 'Secret message app',
 		link: 'https://github.com/brodyd795/secret-message',
 		technologies: ['Next.js', 'Tailwind CSS', 'MySQL'],
-		description: 'In case you ever need to share your Netflix password.'
+		tldr: 'In case you ever need to share your Netflix password.',
+		description: 'I\'ve gone through multiple iterations of this project, learning more and more as I go. It started in Python with Google Sheets as the backend and has now evolved into a single-page Next app powered by automated email scraping and categorization.',
+		image: {
+			src: '/project-images/accounting.jpg',
+			credit: '@tezos'
+		}
 	},
 	{
 		name: 'Covid-19 vaccine appt finder',
 		link: 'https://github.com/brodyd795/covid-vaccine-appointment-notifier',
 		technologies: ['Node.js', 'Twilio', 'Cheerio'],
-		description:
-			'Back when appointments were hard to find. Alerted users of open slots in their area every few minutes.'
+		tldr: 'Back when appointments were hard to find. Alerted users of open slots in their area every few minutes.',
+		description: 'I\'ve gone through multiple iterations of this project, learning more and more as I go. It started in Python with Google Sheets as the backend and has now evolved into a single-page Next app powered by automated email scraping and categorization.',
+		image: {
+			src: '/project-images/accounting.jpg',
+			credit: '@tezos'
+		}
 	},
 	{
 		name: 'Hy-Vee Aisles Online',
 		link: 'https://hy-vee.com/aisles-online',
 		technologies: ['Next.js!'],
-		description:
-			"Hy-Vee's e-commerce platform. Making customers' lives better every day. A team effort, for sure!"
+		tldr: "Hy-Vee's e-commerce platform. Making customers' lives better every day. A team effort, for sure!",
+		description: 'I\'ve gone through multiple iterations of this project, learning more and more as I go. It started in Python with Google Sheets as the backend and has now evolved into a single-page Next app powered by automated email scraping and categorization.',
+		image: {
+			src: '/project-images/accounting.jpg',
+			credit: '@tezos'
+		}
 	}
 ];
