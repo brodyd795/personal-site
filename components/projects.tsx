892e58f96f8e4a1db5171290694cb98d1532973f
Brody Dingel <bdingel@hy-vee.com>
2022-01-18 22:39:58 -0600

    Get projects styling skeleton in better place

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,4 +1,6 @@
 import React, {FC} from 'react';
+import Image from 'next/image';
+
 import {IProject, projects} from '../data/projects';
 import {Heading} from './heading';
 
@@ -14,24 +16,39 @@ const ProjectCard: FC<IProjectCard> = ({
 		target='_blank'
 		rel='noreferrer'
 		data-testid={`project-card-${name}`}
+		className='max-h-52'
 	>
-		<div className='flex flex-col flex-1 justify-between border-zinc-500 border-2 m-1 w-80 h-52 p-2'>
+		<div className='flex flex-col border-zinc-500 border-2 m-1 p-2'>
 			<span className='text-center font-bold text-xl'>{name}</span>
-			<span className='mt-4 mb-auto'>{description}</span>
-			{technologies ? (
-				<span className='italic mt-2'>{technologies.join(', ')}</span>
-			) : null}
+			<div className='flex'>
+				<div className='w-28 h-28'>
+					<Image
+						alt={name}
+						src='/project-images/accounting.jpg'
+						layout='responsive'
+						width={100}
+						height={100}
+						priority
+					/>
+				</div>
+				<div className='flex flex-col flex-1 ml-4'>
+					<span className='mt-4 mb-auto'>{description}</span>
+					{technologies ? (
+						<span className='italic mt-2'>{technologies.join(', ')}</span>
+					) : null}
+				</div>
+			</div>
 		</div>
 	</a>
 );
 
 export const Projects: FC = () => (
 	<div
-		className='min-h-screen w-full bg-emerald-100  flex flex-col items-center'
+		className='w-full py-10 bg-emerald-100 flex flex-col items-center'
 		id='projects'
 	>
 		<Heading text='Projects' />
-		<div className='flex justify-center flex-wrap max-w-screen-lg'>
+		<div className='flex flex-col justify-center flex-wrap max-w-screen-lg'>
 			{projects.map((project) => (
 				<ProjectCard key={project.name} project={project} />
 			))}
