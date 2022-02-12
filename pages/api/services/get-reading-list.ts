94603b530f30933eade2c8e9eabe77c1061e5fc7
Brody Dingel <bdingel@hy-vee.com>
2022-02-12 06:52:14 -0600

    remove unneeded link preview logic

--- pages/api/services/get-reading-list.ts
+++ pages/api/services/get-reading-list.ts
@@ -1,11 +1,9 @@
-import linkPreviewGenerator from 'link-preview-generator';
-
 import {prismaClient} from './prisma-client';
 
 export const getReadingList = async () => {
 	const prisma = prismaClient();
 
-	const rawItemData = await prisma.reading_list.findMany({
+	const items = await prisma.reading_list.findMany({
 		take: 2,
 		orderBy: {
 			date_added: 'desc'
@@ -13,15 +11,5 @@ export const getReadingList = async () => {
 	});
 	await prisma.$disconnect();
 
-	// @ts-ignore
-	const previewData = await Promise.all(
-		rawItemData.map((item) => linkPreviewGenerator(item.url))
-	);
-
-	const finalData = rawItemData.map((item, index) => ({
-		...item,
-		...previewData[index]
-	}));
-
-	return finalData;
+	return items;
 };
