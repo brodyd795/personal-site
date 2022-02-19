4e2087c407ede4bad3b19f392f1cf6fe9fba0d23
Brody Dingel <bdingel@hy-vee.com>
2022-02-19 06:40:04 -0600

    Add button to header

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,8 @@
 import React, {FC} from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import {faAngleDown} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 import backgroundImage from '../public/background.jpg';
 
@@ -20,8 +22,8 @@ export const Header: FC<IHeaderProps> = ({
 		<div className='h-100 w-100 overflow-hidden z-0'>
 			<Image src={backgroundImage} priority layout='fill' placeholder='blur' />
 		</div>
-		<div className='h-100 w-full flex flex-col z-10'>
-			<nav className='p-2 flex justify-end'>
+		<div className='h-100 w-full flex flex-col z-10 items-center'>
+			<nav className='p-2 flex w-full justify-end'>
 				<Link href='/'>
 					{/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
 					<a className={`${linkStyles} mr-auto`}>Home</a>
@@ -35,17 +37,18 @@ export const Header: FC<IHeaderProps> = ({
 					<a className={linkStyles}>Contact</a>
 				</Link>
 			</nav>
-			<div className='flex-1 flex flex-col justify-center mb-80'>
+			<div className='flex-1 flex flex-col justify-center'>
 				<h1 className='text-center text-8xl'>{headerText}</h1>
 				{subHeaderText.map((text) => (
 					<span
 						key={text}
-						className='text-center block first-of-type:mt-16 text-xl'
+						className='text-center block first-of-type:mt-16 text-3xl'
 					>
 						{text}
 					</span>
 				))}
 			</div>
+			<button type={'button'} className={'bg-green-500 mb-80 p-4 animate-bounce'}>Get to know me <FontAwesomeIcon icon={faAngleDown} className='pl-1' /></button>
 		</div>
 	</header>
 );
