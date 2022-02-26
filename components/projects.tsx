c2cae6c8b35934a35f255999b469b744961c93e1
Brody Dingel <bdingel@hy-vee.com>
2022-02-25 22:28:27 -0600

    spice up projects section

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,5 +1,6 @@
-import React, {FC} from 'react';
-import Image from 'next/image';
+import React, {FC, useState} from 'react';
+import {faGithub} from "@fortawesome/free-brands-svg-icons"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 import {IProject, projects} from '../data/projects';
 import {Heading} from './heading';
@@ -10,51 +11,54 @@ interface IProjectCard {
 }
 
 const ProjectCard: FC<IProjectCard> = ({
-	project: {name, link, technologies, description, tldr, image: projectImage}
-}: IProjectCard) => (
-	<a
-		href={link}
-		target='_blank'
-		rel='noreferrer'
-		data-testid={`project-card-${name}`}
-	>
-		<div className='flex flex-col border-zinc-500 bg-sky-100 border-2 my-2 p-2'>
-			<span className='text-center font-bold text-xl'>{name}</span>
-			<div className='flex items-end'>
-				<div className='w-28 h-36 text-center hidden sm:block'>
-					<Image
-						alt={name}
-						src={projectImage.src}
-						layout='responsive'
-						width={100}
-						height={100}
-					/>
-					<span className='text-xs italic'>{`Credit: ${projectImage.credit}`}</span>
+	project: {name, link, technologies, description, tldr}
+}: IProjectCard) => {
+	const [showMore, setShowMore] = useState(false);
+	const lineClampStyles = !showMore ? 'line-clamp-2' : '';
+
+	return (
+		<a
+			href={link}
+			target='_blank'
+			rel='noreferrer'
+			data-testid={`project-card-${name}`}
+		>
+			<div className='flex flex-col border-zinc-500 rounded-lg border-2 my-2 px-2 py-3'>
+				<div className={'flex justify-center relative'}>
+					<h4 className='text-center font-bold text-xl mt-2'>{name}</h4>
+					<FontAwesomeIcon icon={faGithub} className='absolute w-6 h-6 top-3 right-0' />
 				</div>
-				<div className='flex flex-col flex-1 pl-4'>
+				<div className='flex items-end'>
+					<div className='flex flex-col flex-1 pl-4'>
 					<span className='mt-4 mb-auto'>
-						<span className='font-bold'>{'TL;DR: '}</span>
 						{tldr}
 					</span>
-					<span className='mt-2 mb-auto'>
-						<span className='font-bold'>{"What I've learned: "}</span>
-						{description}
-					</span>
-					{technologies ? (
-						<span className='italic mt-2'>
-							<span className='font-bold'>{'Using: '}</span>
-							{technologies.join(', ')}
-						</span>
-					) : null}
+						<div className='mt-4 mb-auto'>
+							<span className='font-bold block'>{"What I learned: "}</span>
+							<span className={`${lineClampStyles} sm:line-clamp-none`}>
+								{description}
+							</span>
+							{!showMore && <button className={'text-slate-500 sm:hidden'} type={'button'} onClick={(e) => {
+								e.preventDefault();
+								setShowMore(true);
+							}}>See all</button>}
+					</div>
+						{technologies ? (
+							<div className={'mt-2'}>
+								<span className='font-bold block'>{'Technologies:'}</span>
+								<span>{technologies.join(', ')}</span>
+							</div>
+						) : null}
+					</div>
 				</div>
 			</div>
-		</div>
-	</a>
-);
+		</a>
+	)
+};
 
 export const Projects: FC = () => (
 	<div
-		className='w-full py-10 flex flex-col items-center'
+		className='w-full py-10 flex flex-col'
 		id='projects'
 	>
 		<Heading text='Projects' />
@@ -62,7 +66,6 @@ export const Projects: FC = () => (
 			{projects.map((project) => (
 				<ProjectCard key={project.name} project={project} />
 			))}
-			<Hr />
 		</div>
 	</div>
 );
