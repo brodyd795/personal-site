6f21b9a0c5de659b93516110d72154b7a577c3a6
Brody Dingel <bdingel@hy-vee.com>
2022-03-07 07:09:18 -0600

    font size in about

--- components/about.tsx
+++ components/about.tsx
@@ -6,6 +6,9 @@ import {Heading} from './heading';
 const H3 = ({children}: {children: JSX.Element | string}) => (
 	<h3 className={'text-white mt-4 children-lg'}>{children}</h3>
 );
+const H2 = ({children}: {children: JSX.Element | string}) => (
+	<h2 className={'text-white mt-4 text-xl'}>{children}</h2>
+);
 const P = ({children}: {children: JSX.Element | string}) => (
 	<p className={'mt-3'}>{children}</p>
 );
@@ -17,7 +20,7 @@ export const About = (): JSX.Element => {
 		<div className='w-full py-10 flex flex-col' id='about'>
 			<Heading text={'About'} />
 			<div className={'mx-4'}>
-				<p className={'mt-4 text-xl'}>TL;DR:</p>
+				<H2>TL;DR:</H2>
 				<ul className={'list-disc ml-4 mt-4 leading-loose text-gray-400'}>
 					<li>Full-stack software engineer in ecommerce at Hy-Vee</li>
 					<li>
@@ -46,7 +49,7 @@ export const About = (): JSX.Element => {
 				</ul>
 				{showMore && (
 					<div className={'mt-4 text-gray-400 leading-relaxed'}>
-						<H3>{`You want to know more? Alright!`}</H3>
+						<H2>{`You want to know more? Alright!`}</H2>
 						<H3>{`My journey into software engineering`}</H3>
 						<P>{`I grew up in small-town Iowa and headed to Iowa State University (Go, Cyclones!) after high school.
                             Didn't quite know what I wanted to do with my life at that point, but I quickly discovered a passion
@@ -54,7 +57,7 @@ export const About = (): JSX.Element => {
 						<P>{`By the end of my first year, I was introduced to programming. And so my career began!`}</P>
 						<P>{`I spent the next three years dabbling in programming while I focused on my studies in Spanish and Linguistics. Mostly Perl and Python, of which I remember almost nothing 😅
                             But my interest continued to grow little by little.`}</P>
-						<P>{`After graduating with my bachelors degrees, I continued my studies at ISU in a Master's program in Applied Linguistics. I was deeply interested in coding by this point, so I declared an emphasis in computational/corpus linguistics.`}</P>
+						<P>{`After graduating with my bachelor's degrees, I continued my studies at ISU in a Master's program in Applied Linguistics. I was deeply interested in coding by this point, so I declared an emphasis in computational/corpus linguistics.`}</P>
 						<P>{`In December 2018, I decided to make a career switch to software engineeering! Fortunately, my major professor had a background in software and agreed to help me in my journey.`}</P>
 						<P>{`My breakthrough opportunity came a few months later when I was asked to teach an introductory course titled "Computers and Language" in the Linguistics program. I knew I wanted to teach my upcoming students how to use regular expressions,
                             so I decided to build a website for that purpose. It took me the full summer of 2019, but I did it!`}</P>
