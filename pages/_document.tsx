94ff10ad58419173d19f3bde3980f8ef00f65031
Brody Dingel <bdingel@hy-vee.com>
2022-03-10 05:51:21 -0600

    update favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,9 +8,27 @@ export default class MyDocument extends Document {
 		return (
 			<Html lang='en'>
 				<Head>
-					<link href='/favicon.ico' rel='icon' />
 					<meta name='description' content="Brody Dingel's personal website" />
 					<meta name='robots' content='follow, index' />
+					<link href='favicons/favicon.ico' rel='shortcut icon' />
+					<link href='favicons/site.webmanifest' rel='manifest' />
+					<link
+						href='favicons/apple-touch-icon.png'
+						rel='apple-touch-icon'
+						sizes='180x180'
+					/>
+					<link
+						href='favicons/favicon-32x32.png'
+						rel='icon'
+						sizes='32x32'
+						type='image/png'
+					/>
+					<link
+						href='favicons/favicon-16x16.png'
+						rel='icon'
+						sizes='16x16'
+						type='image/png'
+					/>
 					<script
 						async
 						src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
