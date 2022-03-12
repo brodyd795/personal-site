2874e50efd316a93881250052c396a874a5aac4d
Brody Dingel <bdingel@hy-vee.com>
2022-03-12 06:46:16 -0600

    in jest.set.tsx too

--- jest.setup.tsx
+++ jest.setup.tsx
@@ -5,7 +5,9 @@ import {ImageProps} from 'next/image';
 import React from 'react';
 import {domains} from './enums/domains';
 
-process.env.VERCEL_ENV = domains.LOCALHOST;
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+process.env.NODE_ENV = domains.LOCALHOST;
 
 jest.mock('next/router');
 jest.mock('next/image', () => (props: ImageProps) => {
