7e8bcd3bb28a50da50f6395109caba25c8a0f62f
Brody Dingel <bdingel@hy-vee.com>
2022-03-12 07:04:58 -0600

    simplify url-helper

--- utils/url-helpers.ts
+++ utils/url-helpers.ts
@@ -9,29 +9,6 @@ export const redirectTo = (redirectToUrl: string): string => {
 	return `/api/auth/login`;
 };
 
-export const getBaseUrl = (): string => {
-	const environment = process.env.NODE_ENV;
-
-	// eslint-disable-next-line no-console
-	console.log({
-		environments,
-		environment,
-		domains,
-		commitRef: process.env.VERCEL_GIT_COMMIT_REF,
-		vercelUrl: process.env.VERCEL_URL
-	});
-
-	if (environment === environments.PRODUCTION) {
-		return `https://${domains.PRODUCTION}`;
-	}
-	if (environment === environments.PREVIEW) {
-		return process.env.VERCEL_GIT_COMMIT_REF === 'dev'
-			? `https://${domains.DEV}`
-			: // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-			  `https://${process.env.VERCEL_URL!}`;
-	}
-
-	return `http://${domains.LOCALHOST}`;
-};
+export const getBaseUrl = (): string => window.location.origin;
 
 export const getRedirectUrl = (): string => `${getBaseUrl()}/api/auth/callback`;
