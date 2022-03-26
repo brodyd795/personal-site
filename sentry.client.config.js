43ad55423f12c8d199d66b22621549bcf6c8cb31
Brody Dingel <38290629+brodyd795@users.noreply.github.com>
2022-03-26 07:50:30 -0500

    Add 100% tracing to Sentry config

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -5,5 +5,6 @@ const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
 Sentry.init({
 	dsn: SENTRY_DSN,
 	enabled: process.env.NODE_ENV !== 'development',
-	environment: process.env.NODE_ENV
+	environment: process.env.NODE_ENV,
+	tracesSampleRate: 1.0
 });
